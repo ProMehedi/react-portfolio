70ca3ddb2718417d02a84129240bbc2011f4c4c5
promehedi <mmhasan563@gmail.com>
2021-08-18 20:25:32 +0600

    Initialize Project

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    id: '1',
+    name: 'Project 1',
+    category: 'html',
+    desc: 'This is project 1',
+    link: 'https://promehedi.com/',
+  },
+  {
+    id: '2',
+    name: 'Project 2',
+    category: 'js',
+    desc: 'This is project 2',
+    link: 'https://promehedi.com/',
+  },
+  {
+    id: '3',
+    name: 'Project 3',
+    category: 'reactjs',
+    desc: 'This is project 3',
+    link: 'https://promehedi.com/',
+  },
+  {
+    id: '4',
+    name: 'Project 4',
+    category: 'mern',
+    desc: 'This is project 4',
+    link: 'https://promehedi.com/',
+  },
+]
+
+export default projects
