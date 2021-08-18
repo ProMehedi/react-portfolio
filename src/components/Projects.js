fa78a6f46ece186a3f0d0e9b0c76ae00584d780d
promehedi <mmhasan563@gmail.com>
2021-08-18 22:19:36 +0600

    Change Data Stucture

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,30 +2,11 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 
 const Projects = ({ data }) => {
-  const categories = []
-  data.forEach((project) => {
-    if (!categories.includes(project.category)) {
-      categories.push(project.category)
-    }
-  })
-
-  const title = (category) => {
-    if (category === 'html') {
-      return 'HTML, CSS / Web Design Projects'
-    } else if (category === 'js') {
-      return 'Javascript Projects'
-    } else if (category === 'react') {
-      return 'React Projects'
-    } else if (category === 'mern') {
-      return 'MERN Projects'
-    }
-  }
-
   return (
     <>
-      {categories.map((category, i) => (
+      {data.map((projects, i) => (
         <div className='mb-5' key={i}>
-          <h4 className='my-3'>{title(category)}</h4>
+          <h4 className='my-3'>{projects.title}</h4>
           <Table striped bordered hover>
             <thead>
               <tr>
@@ -36,27 +17,22 @@ const Projects = ({ data }) => {
               </tr>
             </thead>
             <tbody>
-              {data.map((project, index) => {
-                if (project.category === category) {
-                  return (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{project.name}</td>
-                      <td>{project.desc}</td>
-                      <td>
-                        <a
-                          href={project.link}
-                          target='_blank'
-                          rel='noopener noreferrer'
-                        >
-                          {project.link}
-                        </a>
-                      </td>
-                    </tr>
-                  )
-                }
-                return null
-              })}
+              {projects.data.map((project, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{project.name}</td>
+                  <td>{project.desc}</td>
+                  <td>
+                    <a
+                      href={project.link}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      {project.link}
+                    </a>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         </div>
--- src/data.js
+++ src/data.js
@@ -1,33 +1,33 @@
 const projects = [
   {
-    name: 'Project 1',
-    category: 'html',
-    desc: 'This is project 1',
-    link: 'https://promehedi.com/',
+    title: 'HTML, CSS / Web Design Projects',
+    data: [
+      {
+        name: 'HTML Project 1',
+        desc: 'This is project 1',
+        link: 'https://promehedi.com/',
+      },
+      {
+        name: 'HTML Project 2',
+        desc: 'This is project 2',
+        link: 'https://promehedi.com/',
+      },
+    ],
   },
   {
-    name: 'Project 2',
-    category: 'js',
-    desc: 'This is project 2',
-    link: 'https://promehedi.com/',
-  },
-  {
-    name: 'Project 3',
-    category: 'react',
-    desc: 'This is project 3',
-    link: 'https://promehedi.com/',
-  },
-  {
-    name: 'React Project 2',
-    category: 'react',
-    desc: 'This is react project 2',
-    link: 'https://promehedi.com/',
-  },
-  {
-    name: 'Project 4',
-    category: 'mern',
-    desc: 'This is project 4',
-    link: 'https://promehedi.com/',
+    title: 'Javascript Projects',
+    data: [
+      {
+        name: 'JS Project 1',
+        desc: 'This is project 1',
+        link: 'https://promehedi.com/',
+      },
+      {
+        name: 'JS Project 2',
+        desc: 'This is project 2',
+        link: 'https://promehedi.com/',
+      },
+    ],
   },
 ]
 
