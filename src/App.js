a0e840f93a3ced3258e43124a2054e7562e591ac
promehedi <mmhasan563@gmail.com>
2021-08-18 23:22:09 +0600

    Load Projects from Github-pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Projects from './components/Projects'
 import Screen from './components/Screen'
-import projects from './data'
 
 const App = () => {
+  const [projects, setProjects] = useState([])
+
+  const fetchProjects = async () => {
+    const response = await fetch(
+      'https://promehedi.github.io/react-portfolio/data.json'
+    )
+    const data = await response.json()
+    setProjects(data)
+  }
+
+  useEffect(() => {
+    fetchProjects()
+  }, [])
+
+  if (projects.length === 0) {
+    return <div>Loading...</div>
+  }
+
   return (
     <Screen>
       <Projects data={projects} />
